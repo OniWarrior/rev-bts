0ab8fa81f0f47ec2b6de738500ede47ced1f88ba
Stephen Aranda <aranda.stephen88@gmail.com>
2025-08-01 17:29:23 -0500

    added handler func

--- rev-bts/src/components/navbar.jsx
+++ rev-bts/src/components/navbar.jsx
@@ -1,6 +1,22 @@
+/*
+ * Author: Stephen aranda
+ * File  : navbar.jsx
+ * Desc  : single-file-comp that is the navigation bar for logged out or non-existent
+ *         account. 
+ */
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+    // hook for navigation 
+    const navigate = useNavigate();
+
+    // handler func for home
+    const goHome = () => {
+        navigate('/');
+    }
+
+
 
 
 }

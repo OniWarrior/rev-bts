2e04f265f6d73877dbe8ebba91257e2d1d0a0296
Stephen Aranda <aranda.stephen88@gmail.com>
2025-08-02 22:02:11 -0500

    added text to home page

--- rev-bts/src/components/home.jsx
+++ rev-bts/src/components/home.jsx
@@ -6,6 +6,16 @@ const Home = () => {
     return (
         <div className="home-container">
             <NavBar />
+            <div className="home-header">
+                <h1>Bitcoin Transaction System</h1>
+            </div>
+            <div className="main-text">
+                <p>
+                    Welcome to Bitcoin Transaction System. The app that allows traders and clients to quickly and easily trade in Bitcoin crypto currency with an easy-to-use dashboard.
+                    Traders can buy and sell Bitcoin on behalf of clients or clients can buy and sell directly.
+                    Create a personal account to buy and sell Bitcoin today!
+                </p>
+            </div>
 
 
         </div>

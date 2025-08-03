1f773917fbfc0d46ea8b23875bf1828773863c1d
Stephen Aranda <aranda.stephen88@gmail.com>
2025-08-03 09:43:11 -0500

    created signup css and blank signup func

---
+++ rev-bts/src/components/signup.jsx
@@ -0,0 +1,14 @@
+/*
+ * Author: Stephen Aranda
+ * File  : signup.jsx
+ * Desc  : Single file component for the signup page
+ */
+
+import React from "react";
+import '../styles/signup.css';
+
+const Signup = () => {
+
+}
+
+export default Signup;

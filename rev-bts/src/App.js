3f22752bcce4f52f2f0d50230a597b09dd981d47
Stephen Aranda <aranda.stephen88@gmail.com>
2025-08-03 09:44:38 -0500

    added route to signup

--- rev-bts/src/App.js
+++ rev-bts/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Home from './components/home';
+import Signup from './components/signup';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
 
     </div>

141af43f072f82df2cae004688fd8b2660f7011a
Stephen Aranda <aranda.stephen88@gmail.com>
2025-08-11 19:19:55 -0500

    created state vars and handler

--- rev-bts/src/components/login.jsx
+++ rev-bts/src/components/login.jsx
@@ -7,9 +7,35 @@
 import React from "react";
 import NavBar from "./navbar";
 import useFormValidation from "../hooks/useFormValidation";
+import Login_Form_Schema from '../form-schemas/login-form-schema';
 
 const Login = () => {
 
+    // Initial values and errors for login component text boxes
+    const initialValues = {
+        email: '',
+        password: ''
+    }
+
+    const initialErrors = {
+        email: '',
+        password: ''
+    }
+
+
+    // local state vars for login and errors
+    const [login, errors, setLogin] = useFormValidation(Login_Form_Schema, initialValues, initialErrors);
+
+    // handler for the input change in text boxes
+    const change = (event) => {
+        setLogin(event, Login_Form_Schema);
+    }
+
+    return (
+        <div>
+            <NavBar />
+        </div>
+    )
 }
 
 export default Login;

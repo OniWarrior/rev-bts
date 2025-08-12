58f1f45658a8d5c020462677e238387f8a99b44d
Stephen Aranda <aranda.stephen88@gmail.com>
2025-08-11 19:50:22 -0500

    completed login comp

--- rev-bts/src/App.js
+++ rev-bts/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Home from './components/home';
 import Signup from './components/signup';
+import Login from './components/login';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
 
     </div>
--- rev-bts/src/components/login.jsx
+++ rev-bts/src/components/login.jsx
@@ -8,6 +8,7 @@ import React from "react";
 import NavBar from "./navbar";
 import useFormValidation from "../hooks/useFormValidation";
 import Login_Form_Schema from '../form-schemas/login-form-schema';
+import "../styles/login.css";
 
 const Login = () => {
 
@@ -39,7 +40,60 @@ const Login = () => {
                     <form className="login-form">
                         <h2>Login</h2>
                         <br />
+
+                        <div className='login-input-group'>
+                            <div className="login-label-group">
+                                <label className='label-input-group' htmlFor='email'>
+                                    Email:</label>
+                                <label className="label-input-group" htmlFor="password">
+                                    Password:</label>
+                            </div>
+                            <div className="login-txt-box-group">
+
+
+                                <input className='email-box'
+                                    id='email'
+                                    type='text'
+                                    name='email'
+                                    placeholder='email'
+                                    onChange={change}
+                                />
+
+
+
+                                <div className='errors'>
+                                    <p>{errors.email}</p>
+                                </div>
+
+
+                                <input className="password-code-box"
+                                    id='password'
+                                    type="text"
+                                    name="password"
+                                    placeholder="Password"
+                                    onChange={change}
+                                />
+
+                                <div className="errors">
+                                    <p>{errors.password}</p>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+                        <br></br>
+                        <button
+                            type='submit'
+                            className='login-submit'
+
+
+                        >
+                            Login
+                        </button>
                     </form>
+                    <div className="login-img-container"></div>
                 </div>
             </div>
         </div>

1c8ce91cd64c71769d8726ff47ebdea54742c336
Stephen Aranda <aranda.stephen88@gmail.com>
2025-08-11 19:08:41 -0500

    created blank func

---
+++ rev-bts/src/components/login.jsx
@@ -0,0 +1,15 @@
+/*
+ * Author: Stephen Aranda
+ * File  : login.jsx
+ * Desc  : login component for the users to login to their account
+ */
+
+import React from "react";
+import NavBar from "./navbar";
+import useFormValidation from "../hooks/useFormValidation";
+
+const Login = () => {
+
+}
+
+export default Login;

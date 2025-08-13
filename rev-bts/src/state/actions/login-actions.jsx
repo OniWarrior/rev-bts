9781ecfa7736ff972bfce2bd03bdc30a040809f8
Stephen Aranda <aranda.stephen88@gmail.com>
2025-08-12 19:34:48 -0500

    added strings for redux logger

---
+++ rev-bts/src/state/actions/login-actions.jsx
@@ -0,0 +1,12 @@
+/*
+ * Author: Stephen Aranda
+ * File  : login-actions.jsx
+ * Desc  : action suite(api calls) for login 
+ */
+
+// Strings that are dispatched to reducer function and redux logger
+// indicating start, success, failure of an action(api call).
+const LOGIN_START = "LOGIN_START";
+const LOGIN_FAILURE = "LOGIN_FAILURE";
+const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+

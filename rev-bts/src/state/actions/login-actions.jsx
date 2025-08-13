60b94e52f39186e1a2172ce965c05d0063b6fffd
Stephen Aranda <aranda.stephen88@gmail.com>
2025-08-12 19:53:03 -0500

    completed try catch

--- rev-bts/src/state/actions/login-actions.jsx
+++ rev-bts/src/state/actions/login-actions.jsx
@@ -4,9 +4,42 @@
  * Desc  : action suite(api calls) for login 
  */
 
+import axios from "axios";
+
+
 // Strings that are dispatched to reducer function and redux logger
 // indicating start, success, failure of an action(api call).
 const LOGIN_START = "LOGIN_START";
 const LOGIN_FAILURE = "LOGIN_FAILURE";
 const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+
+
+
+// API call for posting a login of a user
+export const postLogin = (navigate, login) => async (dispatch) => {
+
+    // dispatch the start of the action
+    dispatch({ type: LOGIN_START })
+
+    // execute api call
+    try {
+
+        // execute the api call and store the response 
+        const response = await axios.post("https://bitcoin-transaction-system-be-72349974fde7.herokuapp.com/api/auth/Login", login)
+
+        // dispatch success along with the success payload
+        dispatch({ type: LOGIN_SUCCESS, payload: response.data });
+
+        // store jwt in local storage
+        localStorage.setItem("token", response.data.token);
+
+    } catch (error) {
+
+        // failure dispatch failure
+        dispatch({ type: LOGIN_FAILURE, payload: error.msg });
+
+
+    }
+
+}
 

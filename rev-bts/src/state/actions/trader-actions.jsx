cd0398c7fd677d83c28110ca7fa6fb7df34bf234
Stephen Aranda <aranda.stephen88@gmail.com>
2025-08-14 18:39:47 -0500

    wrote strings for actions types

---
+++ rev-bts/src/state/actions/trader-actions.jsx
@@ -0,0 +1,13 @@
+/*
+ * Author: Stephen Aranda
+ * File  : trader-actions.jsx
+ * Desc  : actions suite for trader user type
+*/
+
+import AxiosWithAuth from "../../components/utils/axios-with-auth";
+
+export const TRADER_START = "TRADER_START";
+export const TRADER_SUCCESS = "TRADER_SUCCESS";
+export const TRADER_FAILURE = "TRADER_FAILURE";
+
+

345f6c4e89fa48710e58a25f8d4f6aa041b5a8b2
Stephen Aranda <aranda.stephen88@gmail.com>
2025-08-14 18:44:42 -0500

    completed api call

--- rev-bts/src/state/actions/trader-actions.jsx
+++ rev-bts/src/state/actions/trader-actions.jsx
@@ -11,3 +11,27 @@ export const TRADER_SUCCESS = "TRADER_SUCCESS";
 export const TRADER_FAILURE = "TRADER_FAILURE";
 
 
+// getCancelLog: get the cancel log for canceled orders
+//@navigate: navigate parameter to navigate to cancel log
+//@dispatch: parameter to dispatch action types and payload
+
+export const getCancelLog = (navigate) => async (dispatch) => {
+    // dispatch start of action
+    dispatch({ type: TRADER_START });
+
+    try {
+
+        // make http request and save response
+        const response = await AxiosWithAuth().get('api/users/cancel-log');
+
+        // dispatch success with response data to payload
+        dispatch({ type: TRADER_SUCCESS, payload: response.data });
+
+        //navigate to cancel log
+        navigate('/trader-dashboard/cancel-log');
+
+    } catch (error) {
+        // dispatch failure with failure message
+        dispatch({ type: TRADER_FAILURE, payload: error.message });
+    }
+}

7f4ee1d99d031bf3a9bf8d21678d46e84f3420df
Stephen Aranda <aranda.stephen88@gmail.com>
2025-10-24 14:38:14 -0500

    created blank func for logged in user

---
+++ rev-bts/src/components/logged-in-navbar.jsx
@@ -0,0 +1,15 @@
+/*
+ * author: Stephen Aranda
+ * File  : logged-in-navbar.jsx 
+ * Desc  : this is the component of a logged in user. The nav bar that is rendered
+ *       : will depend on whether the user is a Client, Trader, or Manager
+ ** */
+import React from "react";
+import { connect } from "react-redux";
+import { useNavigate } from 'react-router-dom';
+
+const LoggedInNav = (props) => {
+
+}
+
+export default 

852886d74f5dd070b5bdaec2f9c9aa40c13b7c24
Stephen Aranda <aranda.stephen88@gmail.com>
2025-10-24 10:23:50 -0500

    added path to render client dash

--- rev-bts/src/App.js
+++ rev-bts/src/App.js
@@ -4,6 +4,8 @@ import Home from './components/home';
 import Signup from './components/signup';
 import Login from './components/login';
 import { Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/utils/protected-route';
+import clientDashBoard from './components/client/client-dashboard';
 
 function App() {
   return (
@@ -13,6 +15,9 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
+        <Route element={<ProtectedRoute />} />
+        <Route path='/client-dashboard' element={<clientDashBoard />} />
+
       </Routes>
 
     </div>

886c69fd74ac55a94a9cb87ad6eb715f557eb5bf
Stephen Aranda <aranda.stephen88@gmail.com>
2025-10-24 10:24:06 -0500

    created blank func for dashboard comp

---
+++ rev-bts/src/components/client/client-dashboard.jsx
@@ -0,0 +1,14 @@
+/*
+ * Author: Stephen Aranda
+ * File  : client-dashboard.jsx 
+ * Desc  : This is the component for the client dashboard of a client user
+ * 
+ * * */
+
+import React from "react";
+
+const clientDashBoard = (props) => {
+
+}
+
+export default clientDashBoard;

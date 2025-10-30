74432857116c5c4e4d4e3e8af35eb41532df598e
Stephen Aranda <aranda.stephen88@gmail.com>
2025-10-30 16:35:44 -0500

    schema for the client buy form

---
+++ rev-bts/src/form-schemas/client-buy-form-schema.jsx
@@ -0,0 +1,24 @@
+import * as yup from 'yup'
+
+const Client_Buy_Form_Schema = yup.object().shape({
+    Bitcoin_balance: yup.number()
+        .required('The amount of bitcoin must be entered.')
+        .typeError('You must specify a number')
+        .min(1, 'The amount cannot be negative'),
+    Bitcoin_price: yup.number(),
+    comm_type: yup.string()
+        .required("Please select a commission type")
+        .oneOf(['USD', 'Bitcoin'], "Commission type must be either 'Bitcoin' or 'USD'"),
+    email: yup.string()
+        .trim()
+        .required()
+        .required("Email/Password is required. Please fill out field")
+        .email()
+    ,
+    password: yup.string()
+        .trim()
+        .required("Email/Password is required.Please fill out field")
+
+})
+
+export default Client_Buy_Form_Schema;

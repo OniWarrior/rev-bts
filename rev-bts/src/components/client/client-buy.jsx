f77422f285dae6e578ae0a0f69e6dfb434c2c87e
Stephen Aranda <aranda.stephen88@gmail.com>
2025-10-30 16:38:57 -0500

    objs for state values and errors

--- rev-bts/src/components/client/client-buy.jsx
+++ rev-bts/src/components/client/client-buy.jsx
@@ -10,11 +10,32 @@ import { connect } from 'react-redux';
 import LoggedInNav from "../logged-in-navbar";
 import { postBuyBitcoin } from "../../state/actions/client-actions";
 import { useNavigate } from "react-router";
+import useFormValidation from '../../hooks/useFormValidation';
+import Client_Buy_Form_Schema from "../../form-schemas/client-buy-form-schema";
 
 const ClientBuy = (props) => {
 
     // local state var for navigation after purchase
     const navigate = useNavigate();
+
+    // initial state object for form
+    const initialValues = {
+        Bitcoin_price: '',
+        Bitcoin_balance: '',
+        email: '',
+        password: '',
+        comm_type: ''
+    }
+
+    // initial errors for form values
+    const initialErrors = {
+        Bitcoin_price: '',
+        Bitcoin_balance: '',
+        email: '',
+        password: '',
+        comm_type: ''
+
+    }
 
 
 

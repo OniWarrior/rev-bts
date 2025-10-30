c6e6fe611167dc38af613634a1699ec564a3ac61
Stephen Aranda <aranda.stephen88@gmail.com>
2025-10-30 16:17:03 -0500

    created blank buy comp

---
+++ rev-bts/src/components/client/client-buy.jsx
@@ -0,0 +1,15 @@
+/*
+ * Author: Stephen Aranda
+ * File  : client-buy.jsx 
+ * Desc  : Single component file for the client buy component
+ *       : Allows the client to make a bitcoin purchase.
+ * */
+
+import React from "react";
+import { connect } from 'react-redux';
+import LoggedInNav from "../logged-in-navbar";
+import { postBuyBitcoin } from "../../state/actions/client-actions";
+
+const ClientBuy = (props) => {
+
+}

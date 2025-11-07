8b7bdfc41eccf13495fa02315b4c51a4c640b897
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-07 09:54:36 -0600

    rendered and styled past orders

---
+++ rev-bts/src/components/client/client-past-orders.jsx
@@ -0,0 +1,48 @@
+import { connect } from "react-redux"
+import PastOrderCard from "./client-past-order-card"
+import LoggedInNav from "../logged-in-navbar"
+import '../../styles/client/client-purchase.css'
+
+const ClientPastOrders = (props) => {
+
+
+    if (!props.client || props.client.length === 0) {
+        return <div>No Past Orders</div>
+    }
+
+    return (
+        <div className="past-orders-container">
+            <LoggedInNav />
+            <div className="past-orders-list">
+                <div className="past-orders-header">
+                    <h1>Past Orders</h1>
+                </div>
+
+                <div className="upper-card-group">
+                    {
+                        props.client.map(order => (
+                            <PastOrderCard key={order.order_id} client={order} />
+
+                        ))
+                    }
+
+                </div>
+
+
+            </div>
+
+        </div>
+    )
+
+}
+
+const mapStateToProps = (state) => {
+    return {
+        client: state.clientReducer.client,
+        loading: state.clientReducer.loading,
+        error: state.clientReducer.error
+    }
+}
+
+
+export default connect(mapStateToProps)(ClientPastOrders)

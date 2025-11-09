6aadf84020310e19c751628ef136f54e2b3dbde5
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-09 16:03:24 -0600

    imported css file

--- rev-bts/src/components/trader/trader-client-search.jsx
+++ rev-bts/src/components/trader/trader-client-search.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import { connect } from "react-redux";
 import LoggedInNavbar from "../logged-in-navbar";
 import { getClient } from "../../state/actions/trader-actions";
+import "../../styles/trader/client-search.css";
 
 const ClientSearch = (props) => {
 
     return (
-        <div className="client-search-container">
+        <div className="client-search">
             <LoggedInNavbar />
             <div className="client-search-main">
+                <div className="client-search-container">
+                    <div className="client-search-header">
+                        <h1>Client Search Form</h1>
+                    </div>
+
+                </div>
+
 
             </div>
         </div>

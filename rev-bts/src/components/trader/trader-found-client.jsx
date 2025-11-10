2dc47c23da54ad98828c72898bb76223ed0e4c03
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-10 09:46:47 -0600

    corrected parameter error

--- rev-bts/src/components/trader/trader-found-client.jsx
+++ rev-bts/src/components/trader/trader-found-client.jsx
@@ -29,8 +29,8 @@ const FoundClient = (props) => {
 
     const goToCancel = (e) => {
         e.preventDefault()
-        props.fetchTransfers(props.trader.client_id)
-        props.fetchTransactions(props.trader.client_id, navigate)
+        props.fetchTransfers(props.trader.client_id);
+        props.fetchTransactions(navigate, props.trader.client_id);
 
 
     }
@@ -42,6 +42,35 @@ const FoundClient = (props) => {
                 <div className="found-client-container">
                     <div className="found-client-header">
                         <h1>Found Client</h1>
+                    </div>
+                    <div className="upper-card-group">
+                        <div className="big-card found-client-info-card">
+                            <div className="big-card-headers">
+                                <h2>First Name</h2>
+                                <h2>Last Name</h2>
+                                <h2>Email</h2>
+                            </div>
+                            <div className="big-card-header-values">
+                                <p>{props.trader.first_name}</p>
+                                <p>{props.trader.last_name}</p>
+                                <p>{props.trader.email}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lower-card-group">
+                        <div className="small-card buy-bitcoin-card">
+                            <div className="btn-t-buy" onClick={goToBuy}></div>
+                            <h2>Buy Bitcoin</h2>
+                        </div>
+                        <div className="small-card sell-bitcoin-card">
+                            <div className="btn-t-sell" onClick={goToSell}></div>
+                            <h2>Sell Bitcoin</h2>
+                        </div>
+                        <div className="small-card cancel-order-card">
+                            <div className="btn-t-cancel-log" onClick={goToCancel}></div>
+                            <h2>Cancel Transfer/Transaction</h2>
+                        </div>
+
                     </div>
                 </div>
             </div>

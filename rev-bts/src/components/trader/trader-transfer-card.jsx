9719d7fd0c17be2af75d4f5b8835ec3ad2c6985a
Stephen Aranda <aranda.stephen88@gmail.com>
2025-11-11 11:31:00 -0600

    created card for transfer

---
+++ rev-bts/src/components/trader/trader-transfer-card.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import '../../styles/trader/transfer-card.css';
+
+
+const TraderTransferCard = (props) => {
+
+    // format date
+    const formattedDate = props.transfer.date.slice(0, 10)
+    return (
+        <div>
+            <div className="trader-transfer-card">
+                <h2>Transfer</h2>
+                <hr />
+                <h3>Transaction id</h3>
+                <p>{props.transfer.transac_id}</p>
+                <h3>Client id</h3>
+                <p>{props.transfer.client_id}</p>
+                <h3>Trader id</h3>
+                <p>{props.transfer.trader_id}</p>
+                <h3>Transfer Amount</h3>
+                <p>{props.transfer.amount_paid}</p>
+                <h3>Date</h3>
+                <p>{formattedDate}</p>
+            </div>
+        </div>
+    )
+
+}
+
+
+export default TraderTransferCard
